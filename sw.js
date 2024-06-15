9bac3f5540eb105f04656bff8da4bdcee30bbba7
Pedro Picanço <51757598+ppicanco93@users.noreply.github.com>
2024-06-15 02:54:37 +0000

    changes to sw

---
+++ sw.js
@@ -0,0 +1,9 @@
+// add event listener
+self.addEventListener("install", (e) => {
+  console.log("Service Worker Installed");
+});
+
+
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker Activated");
+});
