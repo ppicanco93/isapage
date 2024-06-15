eba349aae45f1f24f9ff62b5da0d7fc8c2cbfb64
Pedro Picanço <51757598+ppicanco93@users.noreply.github.com>
2024-06-15 03:29:40 +0000

    changes on sw

--- sw.js
+++ sw.js
@@ -1,9 +1,71 @@
-// add event listener
-self.addEventListener("install", (e) => {
-  console.log("Service Worker Installed");
-});
+var APP_PREFIX = 'IsaPage_'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            
+  '/isapage/',                     
+  '/isapage/index.html',          
+  '/isapage/script.js',          
+  '/isapage/style.css',          
+  '/isapage/Imagens/icon-192.png', 
+  '/isapage/Imagens/icon-512.png', 
+  '/isapage/Imagens/instagram.png', 
+  '/isapage/Imagens/linkedin.png', 
+  '/isapage/Imagens/Logo-topo.png',
+  '/isapage/Imagens/logo.png', 
+  '/isapage/Imagens/telefone.png', 
+  '/isapage/Imagens/logo.ico',
+  '/isapage/Imagens/isa1.JPG',
+  '/isapage/Imagens/isa2.JPG',
+  '/isapage/Imagens/isa3.JPG'
+]
 
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
-self.addEventListener("activate", (e) => {
-  console.log("Service Worker Activated");
-});
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
